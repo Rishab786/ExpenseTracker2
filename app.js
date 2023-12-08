1aa84d2386f2dfdc5a8fa562a2d120d79ba888c2
rishabsingla5@gmail.com <rishabsingla5@gmail.com>
2023-12-08 17:16:09 +0530

    loginFrontendAdded

--- app.js
+++ app.js
@@ -6,8 +6,21 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 const signUpRouter = require("./routes/signUp");
+app.get("/loginAndSignUpcss",(req,res)=>{
+    res.sendFile(path.join(__dirname, "css", "loginAndSignUpPage.css"));
+})
+app.get("/signup", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "signUp.html"));
+  });
+app.post("/signup", signUpRouter);
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "login.html"));
+  });
+app.get("", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "home.html"));
+  });
+  
 
- app.post("/signup", signUpRouter);
 app.listen(3000, () => {
   console.log("server started");
 });
